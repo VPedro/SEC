b1e32e918a4c060e770af460555d88d7dffd74f6
root <root@localhost.localdomain>
2017-03-07 21:56:55 +0000

    .

--- src/pt/ist/sec/proj/Server.java
+++ src/pt/ist/sec/proj/Server.java
@@ -3,6 +3,8 @@ package pt.ist.sec.proj;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.Key;
@@ -17,6 +19,18 @@ public class Server {
 	private static ServerSocket serverSocket;
 	private static Map<ArrayList<String>, String> map;
 	
+	public static void	put(String domain, String username, String password){ 
+		ArrayList<String> list = new ArrayList<String>(); list.add(domain); list.add(username);
+		map.put(list, password);
+	}
+	
+	public static String get(String domain, String username){
+		ArrayList<String> list = new ArrayList<String>(); list.add(domain); list.add(username);
+		return map.get(list);
+	}
+	
+	
+	
 	public static void main(String args[]){
 		map = new HashMap<ArrayList<String>, String>();
 		
@@ -30,7 +44,27 @@ public class Server {
             System.out.println(in.readUTF());
             DataOutputStream out = new DataOutputStream(server.getOutputStream());
             out.writeUTF("Goodbye!");
-            server.close();
+            
+            while(true){
+            	ObjectInputStream objIn = new ObjectInputStream(server.getInputStream());
+            	try {
+					Message m = (Message)objIn.readObject();
+					switch(m.getFunctionName()) {
+						case "save_password":
+							put(m.getDomain(), m.getUsername(), m.getPassword());
+							out.writeUTF("Password saved!");
+							break;
+						case "retrieve_password":
+							out.writeUTF("Retrieving password");
+							out.writeUTF(get(m.getDomain(), m.getUsername()));			
+					}
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+            }
+            
+            
+            //server.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
