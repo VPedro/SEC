d7ca7285d6fed13e5340b608bc0528aff9042e43
root <root@localhost.localdomain>
2017-03-07 21:14:31 +0000

    pushei

--- src/pt/ist/sec/proj/Client.java
+++ src/pt/ist/sec/proj/Client.java
@@ -1,13 +1,23 @@
 package pt.ist.sec.proj;
 
 import java.security.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Client {
 
 	public static void main(String args[]){
 		System.out.println("CLIENT START");
+		
 		Library l = new Library();
+		
 		l.init(null);
+		
+		
+		
+		
+		
 	}
 	
 }
--- src/pt/ist/sec/proj/Server.java
+++ src/pt/ist/sec/proj/Server.java
@@ -6,12 +6,20 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.Key;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.crypto.*;
+
 public class Server {
 
 	private static ServerSocket serverSocket;
+	private static Map<ArrayList<String>, String> map;
 	
 	public static void main(String args[]){
+		map = new HashMap<ArrayList<String>, String>();
+		
 		System.out.println("SERVER STARTING");
 		try {
 			serverSocket = new ServerSocket(85);
