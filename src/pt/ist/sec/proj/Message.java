122a6002a5638e2d0c0cd5ce940b8e502f4309f6
root <root@localhost.localdomain>
2017-03-07 21:38:11 +0000

    Added Message class

---
+++ src/pt/ist/sec/proj/Message.java
@@ -0,0 +1,35 @@
+package pt.ist.sec.proj;
+
+public class Message {
+
+	private String functionName;
+	private String domain;
+	private String username;
+	private String password;
+	
+	public String getFunctionName() {
+		return functionName;
+	}
+	public void setFunctionName(String functionName) {
+		this.functionName = functionName;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
